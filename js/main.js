ea110e49fbf27e376d5d0a2f7b2b05fcbcc7964b
Luis Crespo <lcrespom@gmail.com>
2020-05-23 19:22:06 +0200

    Flip and gravity

--- js/main.js
+++ js/main.js
@@ -1,32 +1,38 @@
 import { Figure } from './figure.js'
 
+const GRAVITY = -0.1
 let canvas = document.getElementById('canvas')
+let height = canvas.height, width = canvas.width
 let ctx = canvas.getContext('2d')
 let figures = []
 
 
 function startGame() {
+    ctx.scale(1, -1)
+    ctx.translate(0, -height)
     figures.push(new Figure({
-        x: 0, y: 200, vx: 1, vy: 0, vangle: 0.01,
+        x: 0, y: 400, vx: 2, vy: 3, vangle: 0.01,
         r: 50, segments: 3, style: '#00FF00'
     }))
     figures.push(new Figure({
-        x: 600, y: 200, vx: -1, vy: 0, vangle: -0.01,
+        x: width, y: 400, vx: -2, vy: 3, vangle: -0.01,
         r: 50, segments: 4, style: '#FF0060'
     }))
     figures.push(new Figure({
-        x: 0, y: 0, vx: 1, vy: 0.5, vangle: -0.01,
+        x: 0, y: 200, vx: 3, vy: 6, vangle: -0.01,
         r: 50, segments: 5, style: '#00FFFF'
     }))
     figures.push(new Figure({
-        x: 600, y: 0, vx: -1, vy: 0.5, vangle: 0.01,
+        x: width, y: 200, vx: -3, vy: 6, vangle: 0.01,
         r: 50, segments: 6, style: '#FFFF00'
     }))
 }
 
 function stepGame() {
-    for (let f of figures)
+    for (let f of figures) {
+        f.vy += GRAVITY
         f.step()
+    }
 }
 
 function drawGame() {
