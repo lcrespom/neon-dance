041f2b16146e176701e2cafe62a6cced2fa64486
Luis Crespo <lcrespom@gmail.com>
2020-05-23 09:10:22 +0200

    Testing neon

--- js/main.js
+++ js/main.js
@@ -12,7 +12,12 @@ function stepGame() {
 }
 
 function drawGame() {
+    ctx.filter = 'blur(4px)'
+    drawPoly(ctx, 200, 200, 50, 4, Math.PI / 8, '#ffee80')
+    ctx.filter = 'none'
     drawPoly(ctx, 200, 200, 50, 4, Math.PI / 8, 'red')
+    ctx.filter = 'none'
+    drawPoly(ctx, 300, 200, 50, 4, Math.PI / 8, 'red')
 }
 
 function animateFrame() {
