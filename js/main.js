6099b5ea7878d133e1e0b6a5f3bd150c978a56e8
Luis Crespo <lcrespom@gmail.com>
2020-05-23 12:00:14 +0200

    Figure class and animation

--- js/main.js
+++ js/main.js
@@ -30,10 +30,6 @@ function stepGame() {
 }
 
 function drawGame() {
-    // neonPoly(ctx, 75, 200, 50, 3, 0, '#00FF00')
-    // neonPoly(ctx, 225, 200, 50, 4, 0 * Math.PI / 8, '#FF0060')
-    // neonPoly(ctx, 375, 200, 50, 5, 0, '#00FFFF')
-    // neonPoly(ctx, 525, 200, 50, 6, 0, '#FFFF00')
     for (let f of figures)
         f.draw(ctx)
 }
