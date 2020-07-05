3c1a06ee88eaf6fcbf93619a19ca2ed36ff20003
Luis Crespo <lcrespom@gmail.com>
2020-07-05 13:14:14 +0200

    Now with nasty looping music

--- js/audio.js
+++ js/audio.js
@@ -1,5 +1,6 @@
 let soundNames = 'pop boing zap gameover'
 let sounds = {}
+let music
 
 function createSounds() {
     let names = soundNames.split(' ')
@@ -8,6 +9,20 @@ function createSounds() {
             src: [`audio/${name}.mp3`]
         })
     }
+}
+
+export function startMusic(loopEnd) {
+    music = new Howl({
+        src: [`audio/music.mp3`],
+        loop: true,
+        volume: 0.4,
+        onend: () => loopEnd(music)
+    })
+    music.play()
+}
+
+export function stopMusic() {
+    music.stop()
 }
 
 export function playSound(name) {
